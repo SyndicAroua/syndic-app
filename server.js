d2e2ab3841c3ddd2a6c7421a064c7babac708891
unknown <syndic.aroua@gmail.com>
2026-05-19 19:03:56 +0100

    real syndic interface

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
+// servir fichiers statiques
+app.use(express.static("public"));
+
+// page login
 app.get("/", (req, res) => {
-  res.send("✅ Syndic app fonctionne !");
+  res.sendFile(path.join(__dirname, "public/login.html"));
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+// dashboard
+app.get("/dashboard", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/dashboard.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running");
 });
